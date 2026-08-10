d16b512737ca811ec0d6c9dc015a36bcc85a03cb
amir <najiamirdesktop@gmail.com>
2026-08-10 11:14:36 +0330

    update todo model

--- shared/types/todo.ts
+++ shared/types/todo.ts
@@ -2,9 +2,11 @@ export type Todo = {
   id: string
   title: string
   completed: boolean
+  description: string
+  priority: "low" | "medium" | "high"
   created_at: string
   updated_at: string
 }
 
-export CreateTodoInput = Pick<Todo, "title">
-export UpateTodoInput = Partial<Pick<Todo, "title" | "completed">>
+export CreateTodoInput = Pick<Todo, "title" | "description" | "priority">
+export UpateTodoInput = Partial<Pick<Todo, "title" | "completed" | "description" | "priority">>
